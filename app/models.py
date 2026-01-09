a9c9b2d1c147e949d45b2ec9fc7f7b93c34818d9
JaeheonSun <jsun0548@uni.sydney.edu.au>
2026-01-09 15:12:09 +0900

    PK related issue resolved

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from . import db, login_manager
 from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
+    __table_args__ = {"sqlite_autoincrement": True}
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
